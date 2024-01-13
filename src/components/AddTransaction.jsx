6874f92b83625472f139dcd0fb620312b43b925d
Tolu Alimi <talimi2000@gmail.com>
2024-01-12 19:33:53 -0600

    Added radio buttons for transaction type

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -4,17 +4,23 @@ import { GlobalContext } from "../context/GlobalState";
 const AddTransaction = () => {
     const [text, setText] = useState("");
     const [amount, setAmount] = useState(0);
+    const [isExpense, setIsExpense] = useState(false);
     const { addTransaction } = useContext(GlobalContext);
 
     function generateID() {
         return Math.floor(Math.random() * 10000000);
     }
+
     const handleTextChange = (event) => {
         setText(event.target.value);
     };
 
     const handleAmountChange = (event) => {
-        setAmount(event.target.value);
+        setAmount(parseFloat(event.target.value));
+    };
+
+    const handleRadioChange = (event) => {
+        setIsExpense(event.target.value === "expense");
     };
 
     const handleSubmit = (event) => {
@@ -23,7 +29,7 @@ const AddTransaction = () => {
         const transaction = {
             id: generateID(),
             text,
-            amount: parseFloat(amount),
+            amount: isExpense ? -amount : amount,
         };
         addTransaction(transaction);
     };
@@ -43,9 +49,43 @@ const AddTransaction = () => {
                     />
                 </div>
                 <div className="form-control">
-                    <label htmlFor="amount">
-                        Amount <br /> (negative - expense, positive - income)
-                    </label>
+                    <label htmlFor="type">Type</label>
+                    <div className="flex items-center me-4">
+                        <input
+                            id="inline-radio"
+                            type="radio"
+                            value="income"
+                            name="inline-radio-group"
+                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                            onChange={handleRadioChange}
+                            checked
+                        />
+                        <label
+                            htmlFor="inline-radio"
+                            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                            Income
+                        </label>
+                    </div>
+                    <div className="flex items-center me-4">
+                        <input
+                            id="inline-2-radio"
+                            type="radio"
+                            value="expense"
+                            name="inline-radio-group"
+                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                            onChange={handleRadioChange}
+                        />
+                        <label
+                            htmlFor="inline-2-radio"
+                            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                            Expense
+                        </label>
+                    </div>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="amount">Amount</label>
                     <input
                         type="number"
                         id="amount"
