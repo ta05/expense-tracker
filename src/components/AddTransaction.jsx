e010455fdaaacc415774b8306439076de810753e
Tolu Alimi <talimi2000@gmail.com>
2024-01-13 13:26:14 -0600

    Fixed radio buttons, so it doesn't default to checking Income when the text or amount inputs are changed

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -60,7 +60,7 @@ const AddTransaction = () => {
                             name="inline-radio-group"
                             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                             onChange={handleRadioChange}
-                            checked
+                            checked={!isExpense}
                         />
                         <label
                             htmlFor="inline-radio"
@@ -77,6 +77,7 @@ const AddTransaction = () => {
                             name="inline-radio-group"
                             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                             onChange={handleRadioChange}
+                            checked={isExpense}
                         />
                         <label
                             htmlFor="inline-2-radio"
